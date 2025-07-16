a49fac54deb214f0977b6d477a806fc718d9eda7
Mofe Adelusimo <mofe.samadelusimo@gmail.com>
2025-07-15 22:52:43 -0500

    Started integration for db functions, need to clear out previous db uses Note database and change to use SQL Alcehmy calls

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from google.auth.exceptions import DefaultCredentialsError
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from database import create_task, init_db, get_all_tasks, update_task, delete_task
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///echo_note.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 db = SQLAlchemy(app)
@@ -82,8 +84,40 @@ def transcribe_audio():
     transcript = " ".join(r.alternatives[0].transcript for r in resp.results)
     return jsonify(transcript=transcript), 200
 
+# List tasks route
+
+@app.route('/api/tasks', methods=['GET'])
+def list_tasks():
+    tasks = get_all_tasks()
+    return jsonify([{
+        "id": t.id,
+        "name": t.name,
+        "completed": t.completed
+    } for t in tasks])
+
+# process tasks route
+@app.route('/api/save_task', methods=['POST'])
+def save_task():
+    data = request.get_json()
+    print("Received JSON:", data)
+    if not data or "tasks" not in data:
+        print("❌ No tasks provided")
+        return jsonify(error='Task name is required'), 400
+    tasks = data["tasks"]
+    count = 0
+    for task_name in tasks:
+        task_text = task_name.get("text")
+        print("➡️ Trying to save task:", task_text)
+        if task_text:
+            create_task(task_text)
+            count += 1
+    print(f"✅ Saved {count} tasks to database")
+    return jsonify(message=f'{count} tasks saved'), 200
+
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        init_db()#initialize the tasks database
     app.run(debug=True)
